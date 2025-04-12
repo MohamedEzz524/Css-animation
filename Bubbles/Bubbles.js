4134514713e2825148eb174fe94daaa57fec99be
MOhamed Elsayed <110637067+MohamedEzz524@users.noreply.github.com>
2025-04-12 02:19:12 +0200

    Add files via upload

---
+++ Bubbles/Bubbles.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./bubbles.css";
+
+export default function Bubbles() {
+  return (
+    <div className="bubbles-animation">
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+      <div>
+        <span className="dot"></span>
+      </div>
+    </div>
+  );
+}
